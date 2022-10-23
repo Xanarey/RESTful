1f0a1db61bd968564c8733fd95baa1da90871f03
Xanarey <doctor.layout@mail.ru>
2022-10-23 10:48:27 +0300

    flyway

--- src/main/resources/db/migration/V0_1__INSERT_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id, firstname, lastname)
-VALUES (1, 'Tom', 'Bronson'),
-       (2, 'Ann', 'Chanson'),
-       (3, 'John', 'Milankovic');
---
+++ src/main/resources/db/migration/V0_4__INSERT_users.sql
@@ -0,0 +1,1 @@
+INSERT INTO postgres.users (id, name) VALUES (1, 'Tom'),(2, 'Ann'),(3, 'Sam');

fc1f3b023f279fb7842cb6c912b86bb61ae7f2b9
Xanarey <doctor.layout@mail.ru>
2022-10-24 21:53:12 +0300

    FileHibernate

--- src/main/java/com/model/File.java
+++ src/main/java/com/model/File.java
@@ -1,4 +1,59 @@
 package com.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "files", schema = "postgres")
 public class File {
+
+    @Id @GeneratedValue(generator = "increment")
+    private Long id;
+    @Column(name = "url")
+    private String url;
+
+    public File() {}
+
+    public File(Long id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof File)) return false;
+        File file = (File) o;
+        return Objects.equals(getId(), file.getId()) && Objects.equals(getUrl(), file.getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUrl());
+    }
 }

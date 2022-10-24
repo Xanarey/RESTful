ac4828ae7ffc32b8d8e3e0fc49f5102382a3f90d
Xanarey <doctor.layout@mail.ru>
2022-10-24 21:56:53 +0300

    HibernateFileRepoImpl

--- src/main/java/com/repository/Hibernate/HibernateFileRepoImpl.java
+++ src/main/java/com/repository/Hibernate/HibernateFileRepoImpl.java
@@ -2,33 +2,69 @@ package com.repository.Hibernate;
 
 import com.model.File;
 import com.repository.FileRepo;
+import com.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.Collections;
 import java.util.List;
 
 public class HibernateFileRepoImpl implements FileRepo {
 
     @Override
-    public File insert(File file) {
-        return null;
+    public File getById(Long id) {
+        File file = new File();
+        try (Session session = HibernateUtil.getSession()){
+            file =  (File) session.createQuery("SELECT d FROM File d WHERE d.id = (:id)").setParameter("id", id).getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 
     @Override
     public List<File> getAll() {
-        return null;
+        List<File> fileList;
+        Transaction transaction;
+        try(Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            fileList = session.createQuery("FROM File", File.class).list();
+            transaction.commit();
+        } catch (Throwable t) {
+            return Collections.emptyList();
+        }
+        return fileList;
     }
 
     @Override
-    public File getById(Long id) {
-        return null;
+    public void deleteById(Long id) {
+        try (Session session = HibernateUtil.getSession()){
+            session.beginTransaction();
+            session.remove(session.get(File.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void deleteById(Long aLong) {
-
+    public File insert(File file) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()){
+            transaction = session.beginTransaction();
+            session.merge(file);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
+        return file;
     }
 
     @Override
     public File update(File file) {
-        return null;
+        insert(file);
+        return file;
     }
 }

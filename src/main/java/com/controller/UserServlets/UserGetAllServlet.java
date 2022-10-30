20b806fb5cfa78b7e04d7c927ec10e4858b14842
timurbrek <scab.rouse@mail.ru>
2022-10-30 17:53:22 +0300

    doGet JSON

--- src/main/java/com/controller/UserServlets/UserGetAllServlet.java
+++ src/main/java/com/controller/UserServlets/UserGetAllServlet.java
@@ -1,15 +1,16 @@
 package com.controller.UserServlets;
 
+import com.google.gson.Gson;
 import com.model.User;
 import com.service.UserService;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(
@@ -18,34 +19,43 @@ import java.util.List;
 )
 public class UserGetAllServlet extends HttpServlet {
 
-    private UserService userService = new UserService();
+    private final UserService userService = new UserService();
+    private final Gson GSON = new Gson();
 
-    private void processRequest(
-            HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        List<User> userList = userService.getAllUsers();
-        request.setAttribute("userGetAllRecord", userList);
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher(
-                "/WEB-INF/jsp/userView/userGetAll-record.jsp");
-        dispatcher.forward(request, response);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("id") != null) {
+            User user = userService.getById(Long.parseLong(request.getParameter("id")));
+            jsonConverter(response, user);
+        }
+        if (request.getParameter("id") == null) {
+            List<User> userList = userService.getAllUsers();
+            jsonConverter(response, userList);
+        }
     }
 
     @Override
-    protected void doGet(
-            HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        processRequest(request, response);
     }
 
     @Override
-    protected void doPost(
-            HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        processRequest(request, response);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    private void jsonConverter(HttpServletResponse response, Object object) throws IOException {
+        String jsonString = GSON.toJson(object);
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(jsonString);
+        out.flush();
     }
 
 }

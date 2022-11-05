ddbea0a5f3855b3f65ea9443b84db1a0b82d8e79
Xanarey <doctor.layout@mail.ru>
2022-11-05 15:26:59 +0300

    1

--- src/main/resources/db/migration/V0_0__users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS postgres.users
-(
-    id integer NOT NULL,
-    name character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-);
